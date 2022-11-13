8bc5c1beba56e9bb1899f7fd26dd5587d36d7ed4
Tom Nuzzarello <tjnuzza@gmail.com>
2022-11-13 16:17:13 -0600

    Press any key test script

---
+++ Press-any-key-test.py
@@ -0,0 +1,6 @@
+# Press any key to continue test
+print('Test script')
+input('Press any key to continue...')
+print('You pressed a key')
+# Looks like it has to be enter only
+# Other buttons appear on screen but don't change behavior
